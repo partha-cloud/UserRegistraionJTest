0e7cfc5b7f69d2db46920236c8644be4f0435d24
partha-cloud <partha11dey@gmail.com>
2021-03-25 23:28:55 +0530

    added UC9

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,16 +4,29 @@ import java.lang.*;
 public class UserRegistration {
 
     private static final Scanner scan = new Scanner(System.in);
-    public static  void main(String[] args) {
+    public static  void main(String[] args)
+
+    {
+        //calling method to validate first Name
         validateUserData();
+        //calling method to validate Last  Name
+        validateUserLastName();
+        //calling method to validate Email Id
+        validEmail();
+        //calling method to validate Mobile No
+        validMobileNo();
+        //calling method to validate Password rule1
+        validPassword();
     }
+
+    //Define method to validate first name
     public static void validateUserData()
     {
         System.out.println("Enter Your First name");
-        String name = scan.next();
+        String name = scan.nextLine();
+
         System.out.println("The first name is: " + name);
         String pattern = "^([A-Z]{1})([a-z]{2,})$";
-
         if( name.matches(pattern))
         {
             System.out.println("Name is Valid");
@@ -23,10 +36,12 @@ public class UserRegistration {
             System.out.println("Name is Invalid");
         }
     }
+    //Define method to validate Last name
     public static void validateUserLastName()
     {
         System.out.println("Enter Your Last name");
-        String name = scan.next();
+        String name = scan.nextLine();
+
         System.out.println("The Last name is: " + name);
         String pattern = "^([A-Z]{1})([a-z]{2,})$";
         if( name.matches(pattern))
@@ -39,12 +54,14 @@ public class UserRegistration {
         }
     }
 
+    //Define method to validate email id
     public static void validEmail()
     {
         System.out.println("Enter Your Email");
-        String name = scan.next();
+        String name = scan.nextLine();
+
         System.out.println("The Email is: " + name);
-        String pattern = "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$";
+        String pattern = "^([a-z]{3,})([\'.\'-\'+]{0,1}[a-z0-9]*)@([a-z]{1,4}).([a-z]{2,3})(^[.]{0,1}[a-z]{2,3}){0,1}$";
         if( name.matches(pattern))
         {
             System.out.println("Name is Valid");
@@ -55,6 +72,7 @@ public class UserRegistration {
         }
     }
 
+    //Define method to validate Mobile No.
     public static void validMobileNo()
     {
         System.out.println("Enter Your Mobile No.");
@@ -72,13 +90,14 @@ public class UserRegistration {
         }
     }
 
+    //Define method to validate Password
     public static void validPassword()
     {
         System.out.println("Enter Your Password.");
         String name = scan.nextLine();
 
         System.out.println("The Password is: " + name);
-        String pattern =  "^[A-Za-z0-9]{8,}$";
+        String pattern =  "^(.*[A-Z]{1,}.*[0-9]{1,}.*[~!@#$%^&*]?[A-Za-z0-9]{5,})$";
         if( name.matches(pattern))
         {
             System.out.println("Name is Valid");
@@ -88,52 +107,5 @@ public class UserRegistration {
             System.out.println("Name is Invalid");
         }
     }
-    public static void passwordAtLeastOneUpperCase()
-    {
-        System.out.println("Enter Your Password.");
-        String name = scan.nextLine();
 
-        System.out.println("The Password is: " + name);
-        String pattern =  "^(.*[A-Z]{1}[A-Za-z0-9]{7,})$";
-        if( name.matches(pattern))
-        {
-            System.out.println("Name is Valid");
-        }
-        else
-        {
-            System.out.println("Name is Invalid");
-        }
-    }
-    public static void validateAlteastOneNumber()
-    {
-        System.out.println("Enter Your Password.");
-        String name = scan.nextLine();
-
-        System.out.println("The Password is: " + name);
-        String pattern = "^(.*[A-Z]{1,}[0-9]{1,}[A-Za-z0-9]{6,})$";
-        if( name.matches(pattern))
-        {
-            System.out.println("Name is Valid");
-        }
-        else
-        {
-            System.out.println("Name is Invalid");
-        }
-    }
-    public static void validateSpecialCharacter()
-    {
-        System.out.println("Enter Your Password.");
-        String name = scan.nextLine();
-
-        System.out.println("The Password is: " + name);
-        String pattern = "^(.*[A-Z]{1,}.*[0-9]{1,}.*[~!@#$%^&*]?[A-Za-z0-9]{5,})$";
-        if( name.matches(pattern))
-        {
-            System.out.println("Name is Valid");
-        }
-        else
-        {
-            System.out.println("Name is Invalid");
-        }
-    }
 }
